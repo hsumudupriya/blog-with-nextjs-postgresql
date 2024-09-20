81c1feb147ad2cbec319f650a2d0e5ad65bddf9d
Hasanta Sumudupriya <hsumudupriya@gmail.com>
2024-09-20 12:06:11 +0530

    About & Contact pages are created.

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function page() {
+    return (
+        <>
+            <h1>About</h1>
+            <p>
+                Elit voluptate occaecat non incididunt tempor fugiat ipsum
+                consectetur duis non velit tempor aliqua. Occaecat et ipsum
+                ullamco pariatur culpa. Aute quis labore est irure cupidatat
+                elit occaecat id reprehenderit est laborum dolore sunt. Amet
+                ullamco ipsum elit ipsum excepteur qui amet nostrud. Commodo
+                amet sint dolore cupidatat Lorem consequat minim.
+            </p>
+        </>
+    );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+export default function page() {
+    return (
+        <>
+            <h1>Contact</h1>
+            <p>
+                Dolore eiusmod nisi sit consequat quis consectetur dolor velit
+                occaecat ullamco cupidatat aute et Lorem. Sunt ipsum Lorem
+                fugiat cillum. Adipisicing do laborum cupidatat irure excepteur
+                adipisicing occaecat voluptate. In officia id laborum ex
+                proident do aliquip eiusmod nulla ut ipsum cillum laboris. Sint
+                ut enim enim officia amet nisi consequat.
+            </p>
+        </>
+    );
+}
